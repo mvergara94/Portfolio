3b5f5f8e7b72e4ac887b27b595ff0c47f7c67d59
mvergara94 <mariovergaralorena@gmail.com>
2023-06-14 09:00:26 -0300

    Colocando animações nas sections

--- src/js/main.js
+++ src/js/main.js
@@ -100,3 +100,24 @@ const header = document.querySelector('header');
 window.addEventListener('scroll', function () {
   header.classList.toggle('sticky', window.scrollY > 50);
 });
+
+// parallax
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show-items');
+    } else {
+      entry.target.classList.remove('show-items');
+    }
+  });
+});
+
+const scrollScale = document.querySelectorAll('.scroll-scale');
+scrollScale.forEach(el => observer.observe(el));
+
+const scrollBottom = document.querySelectorAll('.scroll-bottom');
+scrollBottom.forEach(el => observer.observe(el));
+
+const scrollTop = document.querySelectorAll('.scroll-top');
+scrollTop.forEach(el => observer.observe(el));
